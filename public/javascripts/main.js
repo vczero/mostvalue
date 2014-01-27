76d48571816413a8b9387144d91adc238946bb9c
王利华 <i-wanglihua@autonavi.com>
2014-01-27 16:10:29 +0800

    add chart

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,89 @@
+window.onload=function(){
+    var options={
+        height:130,
+        width:700,
+        maxY:90,
+        id:'svg_chart',
+        ys:[6,60,80,90,90,6,60,80,90,90,6,60,80,90,90],
+        color:['#8BBC21','#2F7ED8','#0D233A','green','red','blue','red','green','blcak','red','blue','red','green','blcak','red']
+    };
+    drawSvgBar(options);
+};
 
+//--------------------------柱状图绘制----------------------------------
+//转屏幕坐标
+function toScreenY(maxy,y){
+    return maxy-y;
+}
+//批量转屏幕坐标
+function toBatchY(maxy,ys){
+    var newYs=[];
+    for(var i=0; i<ys.length;i++){
+        newYs.push(toScreenY(maxy,ys[i]));
+    }
+    return newYs;
+}
+//定制画布，绘制
+function drawSvgBar(options){
+    var height=options.height;//画布
+    var width=options.width;//画布
+    var maxY=options.maxY;//传入的最大值
+    var id=options.id;
+    var ys=options.ys;//传入的直角坐标系的高度值
+    var color=options.color;
+    var DIS=12;//矩形宽
+    var DIS_RECT=30;//柱状间距
+    var RATE=0.95;//转换比率
+    var svg=document.getElementById(id);
+    //批量转屏幕坐标
+    var newys;
+    var convert_new=[];
+    if(maxY<=height){
+        newys=toBatchY(height,ys);
+    }
+    else{
+        for(i=0;i<ys.length;i++){
+            convert_new.push(ys[i]*(height/maxY));
+        }
+        newys=toBatchY(height,convert_new);
+        console.log(newys);
+    }
+    for(var i=0;i<newys.length;i++){
+        var rect=document.createElementNS('http://www.w3.org/2000/svg','rect');
+        rect.setAttributeNS(null,'x',DIS_RECT*(i+1)+DIS*i);
+        rect.setAttributeNS(null,'y',newys[i]);
+        rect.setAttributeNS(null,'width',DIS);
+        rect.setAttributeNS(null,'height',maxY<=height?ys[i]:convert_new[i]);
+        rect.setAttributeNS(null, 'fill','url(#orange_red)');
+        rect.setAttributeNS(null,'stroke-width','1');
+
+        var text=document.createElementNS('http://www.w3.org/2000/svg','text');
+        text.setAttributeNS(null,'x',DIS_RECT*(i+1)+DIS*i);
+        text.setAttributeNS(null,'y',newys[i]-3);
+        text.textContent=ys[i];
+        text.setAttributeNS(null,'font-size','12');
+        text.setAttributeNS(null,'color','#2F7ED8');
+
+        svg.appendChild(text);
+        svg.appendChild(rect);
+    }
+}
+//绘制折线
+function drawPolyLine(id,point){
+    var svg=document.getElementById(id);
+    var polyline=document.createElementNS("http://www.w3.org/2000/svg",'polyline');
+
+    polyline.setAttribute('points','20,130,40,40,100,20');
+    polyline.setAttribute('fill','#FFFFFF');
+    polyline.setAttribute('stroke','#478CDB');
+    polyline.setAttribute('stroke-width','2');
+    svg.appendChild(polyline);
+}
+
+function aa(value){
+    alert(value);
+}
+//---------------------------------------------------------------------
 function header_input_click(){
     var input=document.getElementById('header_input');
     input.value='';
@@ -29,15 +114,5 @@ function show_个人信息分析(){
     specialInfo.style.visibility='hidden';
 }
 
-function toScreenY(maxy,y){
-    return maxy-y;
-}
 
-function toBatchY(maxy,ys){
-    var newYs=[];
-    for(var i=0; i<ys.length;i++){
-        newYs.push(toScreenY(maxy,y));
-    }
-    return newYs;
-}
 
